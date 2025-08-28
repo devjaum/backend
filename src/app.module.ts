8983c7e0a660d8393a9181a0e71615914b380c82
João Carlos <devjaum@gmail.com>
2025-08-28 18:27:52 -0300

    Resolvendo erro

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ChatGateway } from './chat/chat.gateway';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
+      port: parseInt(process.env.DB_PORT ?? '5432'),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
